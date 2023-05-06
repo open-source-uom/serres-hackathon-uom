0b501d6eaa18412876da75d72f4d361832b0b657
nGeorgit <n.georgitsogiannis@gmail.com>
2023-05-06 12:26:00 +0300

    90 deg rotation

--- backend/core/rotation.py
+++ backend/core/rotation.py
@@ -2,12 +2,14 @@ import numpy as np
 import math
 
 
-#rotate shape
+#rotate shape 90 deg clockwise
 def rotate_shape(shape: np.array):
-    rotation_matrix = np.array([[0 -1], [1, 0]])
+    rotation_matrix = np.array([[0, -1], [1, 0]])
+    rotated_matrix = []
     for i in shape:
-        print(i)
+        rotated_matrix.append(np.dot(i, rotation_matrix))
+    return rotated_matrix
 
 
 shape = np.array([[ 0, 0], [ 1,  0], [ 0, -1], [ 2,  0]])
-rotate_shape(shape)
+print(rotate_shape(shape))
