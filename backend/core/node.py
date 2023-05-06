37cc68e514772803749fc6b93820e4cc8edb8c85
nGeorgit <n.georgitsogiannis@gmail.com>
2023-05-06 20:06:27 +0300

    implemented shape to work with nodes

--- backend/core/node.py
+++ backend/core/node.py
@@ -1,5 +1,9 @@
 from typing import Tuple, List, Callable, Any
 class Node():
-    cords: Tuple[int, int]
     def __init__(self, cords: Tuple[int, int], symbol: str):
-        self.cords = cords
+        self.coords: Tuple[int, int] = cords
+        self.symbol: str = symbol
+    def get_coords(self) -> Tuple[int, int]:
+        return self.coords
+    def set_coords(self, coords):
+        self.coords = coords
