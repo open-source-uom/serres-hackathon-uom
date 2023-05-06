ca8024c2a20d613b61b1089403a41787d3833bcd
George David Apostolidis <geoapostolidis999@gmail.com>
2023-05-07 00:06:12 +0300

    Fixed no 14 problem

--- frontend/src/components/Rotate.jsx
+++ frontend/src/components/Rotate.jsx
@@ -6,9 +6,10 @@ const Rotate = () => {
     const isRotated = useStore(state => state.isRotated);
     const setIsRotated = useStore(state => state.setIsRotated);
 
+    //mhn to aggiksei kapoios, leitourgei for some reason, idk einai 12 to bradu pleon
     const handleToggle = () => {
         setIsChecked(!isChecked);
-        setIsRotated(isChecked);
+        setIsRotated(!isChecked);
         console.log("isRotated: ", isRotated);
     };
 
