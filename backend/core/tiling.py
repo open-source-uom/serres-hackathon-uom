cd1a61f071d876f68daef0b27f33a7ce183c291e
Georgios Fakidis <geonfak76@gmail.com>
2023-05-06 11:42:12 +0300

    implemented all letters coords

--- backend/core/tiling.py
+++ backend/core/tiling.py
@@ -1,18 +1,22 @@
 import collections
 from functools import reduce
-
+import time
 import numpy as np
-from typing import List, Tuple
+from typing import List, Tuple, Callable, Any
 
+# 9] Create a function that calculates (in milliseconds, ms) the execution time of any other function.
+def measure_time(func: Callable[..., Any], *args: Tuple[Any], **kwargs: Any) -> Any:
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    running_time = end_time - start_time
+    print(f"Function {func.__name__} took {running_time:.6f} seconds to run.")
+    return result
 
-# shape_to_coords_dict = {"L":np.array(np.array()),"F":np.array(np.array()),
-#                         "I":np.array(np.array()),"N":np.array(np.array()),
-#                         "P":np.array(np.array()),"T":np.array(np.array()),
-#                         "U":np.array(np.array()),"V":np.array(np.array()),
-#                         "W":np.array(np.array()),"X":np.array(np.array()),
-#                         "Y":np.array(np.array()),"Z":np.array(np.array()),}
-
-
+# 10] Create a program that finds each and every (unique) way to fill a Canvas (with or without black cells)
+# with the maximum number of Shapes given (with or without rotation and flipping allowed).
+def find_ways_to_fill_canvas():
+    pass
 
 # Q2
 def generate_all_shapes():
@@ -21,16 +25,90 @@ def generate_all_shapes():
 
 
 class Shape():
-    shape_to_coords_dict = {"L": np.array(np.array(0)), "F": np.array(np.array(0)),
-                            "I": np.array(np.array(0)), "N": np.array(np.array(0)),
-                            "P": np.array(np.array(0)), "T": np.array(np.array(0)),
-                            "U": np.array(np.array(0)), "V": np.array(np.array(0)),
-                            "W": np.array(np.array(0)), "X": np.array(np.array(0)),
-                            "Y": np.array(np.array(0)), "Z": np.array(np.array(0)), }
+    # shapes = {
+    #     "F": np.array([0, 0],[0, -1],[1, -1],[0, 1],[-1, 0]),
+    #     "I": np.array([0, 0],[0, -1],[0, -2],[0, 1],[0, 2],),
+    #     "N": [
+    #         [0, 0],
+    #         [-1, 0],
+    #         [-1, 1],
+    #         [0, -1],
+    #         [0, -2],
+    #     ],
+    #     "P": [
+    #         [0, 0],
+    #         [0, -1],
+    #         [1, -1],
+    #         [1, 0],
+    #         [0, 1],
+    #     ],
+    #     "T": [
+    #         [0, 0],
+    #         [1, 0],
+    #         [-1, 0],
+    #         [0, 1],
+    #         [0, 2],
+    #     ],
+    #     "U": [
+    #         [0, 0],
+    #         [-1, 0],
+    #         [1, 0],
+    #         [1, -1],
+    #         [-1, -1],
+    #     ],
+    #     "V": [
+    #         [0, 0],
+    #         [0, -1],
+    #         [0, -2],
+    #         [-1, 0],
+    #         [-2, 0],
+    #     ],
+    #     "W": [
+    #         [0, 0],
+    #         [-1, 0],
+    #         [-1, -1],
+    #         [0, 1],
+    #         [1, 1],
+    #     ],
+    #     "X": [
+    #         [0, 0],
+    #         [1, 0],
+    #         [-1, 0],
+    #         [0, 1],
+    #         [0, -1],
+    #     ],
+    #     "Y": [
+    #         [0, 0],
+    #         [-1, 0],
+    #         [0, -1],
+    #         [0, 1],
+    #         [0, 2],
+    #     ],
+    #     "Z": [
+    #         [0, 0],
+    #         [0, -1],
+    #         [-1, -1],
+    #         [0, 1],
+    #         [1, 1],
+    #     ]
+    # }
+    shape_to_coords_dict = {"F": np.array([0, 0],[0, -1],[1, -1],[0, 1],[-1, 0]),
+        "I": np.array([0, 0], [0, -1], [0, -2], [0, 1], [0, 2]),
+        "N": np.array([0, 0],[-1, 0],[-1, 1],[0, -1],[0, -2]),
+        "P": np.array([0, 0],[0, -1],[1, -1],[1, 0],[0, 1]),
+        "T": np.array([0, 0],[1, 0],[-1, 0],[0, 1],[0, 2]),
+        "U": np.array([0, 0],[-1, 0],[1, 0],[1, -1],[-1, -1]),
+        "V": np.array([0, 0],[0, -1],[0, -2],[-1, 0],[-2, 0]),
+        "W": np.array([0, 0],[-1, 0],[-1, -1],[0, 1],[1, 1]),
+        "X": np.array([0, 0],[1, 0],[-1, 0],[0, 1],[0, -1]),
+        "Y": np.array([0, 0],[-1, 0],[0, -1],[0, 1],[0, 2]),
+        "Z": np.array([0, 0],[0, -1],[-1, -1],[0, 1],[1, 1])
+    }
     def __init__(self,letter:str):
-        if(letter not in self.shape_to_coords_dict):raise ValueError
+        if letter not in self.shape_to_coords_dict: raise ValueError
+        self.value = letter.upper()
         self.coords = self.shape_to_coords_dict[letter]
-        self.origin:Tuple[int,int] = 0,0
+        self.origin: Tuple[int, int] = 0, 0
         pass
 
     # mporei na einai kai degrees tha doume
@@ -65,6 +143,7 @@ class Canvas():
 
     # to megethos den mas apasxolei sto backend
     def __init__(self,lines:int,rows:int):
+
         self.shapes_placed:List[Shape] = []
         enough_empty_cells = [self.empty_cell_char for i in range(lines*rows)]
         # den einai swsto auto
@@ -111,7 +190,19 @@ class Canvas():
         return the_sum
         pass
     def get_holes(self) -> List[Tuple[int,int]]:
-        pass
+        holes = []
+        rows,cols = self.matrix.shape
+        for row_index in range(rows):
+            for col_index in range(cols):
+                elem = self.matrix[row_index][col_index]
+                if elem == self.hole_cell_char:
+                    print(elem)
+                    holes.append((row_index,col_index))
+
+
+        return holes
+
+    # Calculate Parameter of shape
     def calculate_parameter_of_shape(self,s1:Shape) -> int:
         pass
 
