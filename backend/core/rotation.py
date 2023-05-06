650a6556c9fce2dffbdcc8ac3b01571798ce7891
nGeorgit <n.georgitsogiannis@gmail.com>
2023-05-06 13:03:01 +0300

    created a manipulator class that rotates the shape

--- backend/core/rotation.py
+++ backend/core/rotation.py
@@ -1,15 +1,29 @@
 import numpy as np
-import math
 
+class manipullator:
+    #a matrix that contains the rotation matrixes, 1:flip vertically, 2:flip horizontaly
+    rotation_matrixes = np.array([
+        [
+            [0, -1], #0:90 clockwise rotation
+            [1, 0]
+        ],
+        [
+            [1, 0], #1:flip vertically
+            [0, -1]
+        ],
+        [
+            [-1, 0], #2:flip horizontaly
+            [0, 1]
+        ]
+    ]
+    )
+    def rotate_shape(self, shape: np.array, rotation: int):
+        rotated_matrix = []
+        for i in shape:
+            rotated_matrix.append(np.dot(i, self.rotation_matrixes[rotation]))
 
-#rotate shape 90 deg clockwise
-def rotate_shape(shape: np.array):
-    rotation_matrix = np.array([[0, -1], [1, 0]])
-    rotated_matrix = []
-    for i in shape:
-        rotated_matrix.append(np.dot(i, rotation_matrix))
-    return rotated_matrix
-
+        return rotated_matrix
 
 shape = np.array([[ 0, 0], [ 1,  0], [ 0, -1], [ 2,  0]])
-print(rotate_shape(shape))
+man = manipullator()
+print(man.rotate_shape(shape, 2))
